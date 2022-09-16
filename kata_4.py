145c12b8a3fb5c8d837294c8aee08d45e682c074
Jia T <66760300+jia-von@users.noreply.github.com>
2022-09-15 20:35:12 -0600

    Update kata_4.py

--- kata_4.py
+++ kata_4.py
@@ -1,4 +1,7 @@
-from operator import index, indexOf
+# Complete the solution so that the function will break up camel casing, using a space between words.
+# "camelCasing"  =>  "camel Casing"
+# "identifier"   =>  "identifier"
+# ""             =>  ""
 import re
 
 reg_ex = re.compile('([A-Z])')
